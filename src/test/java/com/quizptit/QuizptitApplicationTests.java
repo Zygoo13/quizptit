ec2c239d1298b4056b1ccd6b6e3464a382592475
Hoang Quoc Anh <AnhHQ.B23CN022@stu.ptit.edu.vn>
2026-03-27 23:21:57 +0700

    init

---
+++ src/test/java/com/quizptit/QuizptitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizptit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizptitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
