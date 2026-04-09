52ec6a5ee0fbcfbfefce6acbe879a6c4265a6141
nam. <namdh.2804@gmail.com>
2026-04-10 04:46:49 +0700

    fix RandomQuiz not published

--- src/main/java/com/quizptit/attempt/service/impl/AttemptServiceImpl.java
+++ src/main/java/com/quizptit/attempt/service/impl/AttemptServiceImpl.java
@@ -19,6 +19,7 @@ import com.quizptit.review.dto.ReviewItemResult;
 import com.quizptit.review.service.ReviewService;
 import com.quizptit.quiz.entity.Quiz;
 import com.quizptit.quiz.entity.QuizQuestion;
+import com.quizptit.quiz.entity.enums.QuizType;
 import com.quizptit.quiz.repository.QuizQuestionRepository;
 import com.quizptit.quiz.repository.QuizRepository;
 import com.quizptit.user.entity.User;
@@ -55,7 +56,7 @@ public class AttemptServiceImpl implements AttemptService {
                 Quiz quiz = quizRepository.findById(quizId)
                                 .orElseThrow(() -> new RuntimeException("Không tìm thấy bài Quiz"));
 
-                if (!quiz.getIsPublished() && !CurrentUserUtils.isAdmin()) {
+                if (quiz.getQuizType() == QuizType.MANUAL && !quiz.getIsPublished()) {
                         throw new RuntimeException("Bài Quiz này chưa được mở");
                 }
 
